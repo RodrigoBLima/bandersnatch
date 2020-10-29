c3bc91e098c715995f569f4cfbaf93d6855ec9f2
Rodrigo Barbosa <rodrigo.lima@agriconnected.com>
2020-10-29 11:46:09 -0300

    day four

--- public/bandersnatch/src/VideoPlayer.js
+++ public/bandersnatch/src/VideoPlayer.js
@@ -1,11 +1,14 @@
 class VideoMediaPlayer {
-  constructor({ manifestJson, network }) {
+  constructor({ manifestJson, network, videoComponent }) {
     this.manifestJson = manifestJson;
+    this.videoComponent = videoComponent;
     this.network = network;
     this.videoElement = null;
     this.sourceBuffer = null;
     this.selected = {};
     this.videoDuration = 0;
+    this.activeItem = {};
+    this.selections = {};
   }
 
   initializeCodec() {
@@ -45,14 +48,32 @@ class VideoMediaPlayer {
       mediaSource.duration = this.videoDuration;
 
       await this.fileDownload(selected.url);
+
+      setInterval(this.waitForQuestions.bind(this), 200);
     };
   }
 
+  waitForQuestions() {
+    const currentTime = parseInt(this.videoElement.currentTime);
+
+    const option = this.selected.at === currentTime;
+
+    if (!option) return;
+
+    if (this.activeItem.url === this.selected.url) return; // EVITAR QUE O MODAL APAREÇA 2X OU MAIS
+
+    this.videoComponent.configureModal(this.selected.options);
+
+    this.activeItem = this.selected;
+  }
+
   async fileDownload(url) {
+    const fileResolution = await this.currentFileResolution();
+
     const prepareUrl = {
       url,
       fileResolutionTag: this.manifestJson.fileResolutionTag,
-      fileResolution: 360,
+      fileResolution,
       hostTag: this.manifestJson.hostTag,
     };
     const finalUrl = this.network.parseManifestUrl(prepareUrl); // console.log('finalUrl',finalUrl)
@@ -61,14 +82,23 @@ class VideoMediaPlayer {
 
     const data = await this.network.fetchFile(finalUrl);
 
-    return this.processBufferSegments(data)
+    return this.processBufferSegments(data);
   }
+
+  nanageLag(selected) {
+    if (!!~this.selections.indexOf(selected.url)) {
+      selected.at += 5;
+      return;
+    }
+    this.selections.push(selected.url);
+  }
+
   setVideoPlayerDuration(finalUrl) {
     const bars = finalUrl.split("/");
 
     const [name, videoDuration] = bars[bars.length - 1].split("-");
 
-    this.videoDuration = videoDuration;
+    this.videoDuration = parseFloat(videoDuration);
   }
 
   async processBufferSegments(allSegments) {
@@ -87,5 +117,35 @@ class VideoMediaPlayer {
       sourceBuffer.addEventListener("updateend", updateEnd);
       sourceBuffer.addEventListener("error", reject);
     });
+  }
+
+  async currentFileResolution() {
+    const LOWEST_RESOLUTION = 144;
+
+    const prepareUrl = {
+      url: this.manifestJson.finalizar.url,
+      fileResolution: LOWEST_RESOLUTION,
+      fileResolutionTag: this.manifestJson.fileResolutionTag,
+      hostTag: this.manifestJson.hostTag,
+    };
+
+    const url = this.network.parseManifestUrl(prepareUrl);
+    return this.network.getProperResolution(url);
+  }
+
+  async nextChunk(data) {
+    const key = data.tolowerCase();
+    const selected = this.manifestJson[key];
+
+    this.selected = {
+      ...selected,
+      // AJUSTA O TEMPO QUE O MODAL VAIA APARECER
+      at: parseInt(this.videoElement.currentTime + selected.at),
+    };
+
+    this.nanageLag(this.selected);
+    // DEIXA O RESTANDTE DO VIDEO RODAR ENQUANTO BAIXA O NOVO VIDEO
+    this.videoElement.play();
+    await this.fileDownload(selected.url);
   }
 }
